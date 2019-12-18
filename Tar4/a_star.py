f006b0a46c4d873f59707e0c584e244d46ad98ed
Shahaf Shavit <shahaf@ownbackup.com>
2019-12-18 16:19:35 +0200

    Shahaf fixes for readability - same logic

--- Tar4/a_star.py
+++ Tar4/a_star.py
@@ -1,27 +1,33 @@
+# Written by:
+# Sapir Holzman 318265097
+# Shahaf Shavit 314967324
+
+
 import sys
 import copy
 
-from Graph import Graph, Vertex 
+from Tar4.Graph import Graph, AStarGraphVertex
+
 
 def a_star(graph, s):
+    S = []
     distance_for_adj, pi = initialize_single_source(graph, s)
-    S = Graph()
     q = copy.deepcopy(graph.vertices)
 
     while len(q) > 0:
         u = extract_min(q, distance_for_adj)
-        S.add_vertex(u)
+        S.append(u)
         for v in u.adjacent:
-            relax(u, q[v["id"]], distance_for_adj, pi)
+            relax(u, q[v['id']], distance_for_adj, pi)
 
-    print_a_star_output(S, s, pi, distance_for_adj)
+    print_a_star_output(graph, s, pi, distance_for_adj)
 
 
 def initialize_single_source(graph, s):
-    distance_for_adj = { v: sys.maxsize for v in graph.keys()}
+    distance_for_adj = {v_id: sys.maxsize for v_id in graph.vertices_ids}
     distance_for_adj[s.id] = 0
 
-    pi = {v: None for v in graph.keys()}
+    pi = {v_id: None for v_id in graph.vertices_ids}
 
     return distance_for_adj, pi
 
@@ -29,19 +35,19 @@ def initialize_single_source(graph, s):
 def extract_min(q, distance_for_adj):
     min_vertex = None
     min_distance = sys.maxsize
-    
-    for v in q:
-        if distance_for_adj[v] + q[v].weight < min_distance:
-            min_distance = distance_for_adj[v] + q[v].weight 
-            min_vertex = q[v]
-    if min_vertex.id in(q):    
+
+    for v_id in q.keys():
+        if distance_for_adj[v_id] + q[v_id].heuristic_weight < min_distance:
+            min_distance = distance_for_adj[v_id] + q[v_id].heuristic_weight
+            min_vertex = q[v_id]
+    if min_vertex:
         del q[min_vertex.id]
 
     return min_vertex
 
 
 def relax(u, v, distance_for_adj, pi):
-    if distance_for_adj[v.id] > distance_for_adj[u.id] + u.get_adj_weight(v) + v.weight:
+    if distance_for_adj[v.id] > distance_for_adj[u.id] + u.get_adj_weight(v) + v.heuristic_weight:
         distance_for_adj[v.id] = distance_for_adj[u.id] + u.get_adj_weight(v)
         pi[v.id] = u
 
@@ -49,27 +55,27 @@ def relax(u, v, distance_for_adj, pi):
 def print_a_star_output(graph, s, pi, distance_for_adj):
     print('a star results - find shortest paths from root {}'.format(s.id))
 
-    for v in graph.keys():
+    for v in graph.vertices_ids:
         if v != s.id:
-            print('vertex {} has distance of {} from the root {} and pi: {}.'
-                .format(v, distance_for_adj[v], s.id, pi[v].id))
+            print('Vertex {} has distance of {} from the source vertex and pi: {}.'
+                  .format(v, distance_for_adj[v], pi[v].id))
         else:
-            print('vertex {} is root vertex and has distance of {} from himself and {} pi.'
-                .format(v, distance_for_adj[v], pi[v]))
-    print('The End :)')
+            print('Vertex {} is source vertex and has distance of {} from itself and {} pi.'
+                  .format(v, distance_for_adj[v], pi[v]))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     input_graph = Graph()
-    heuristic_weight_dict ={ 'A': 7, 'B': 7, 'C': 5, 'D': 3.5, 'E': 6, 'F': 3, 'H': 0, 'S': 9}
+    heuristic_weight_dict = {'A': 7, 'B': 7, 'C': 5, 'D': 3.5, 'E': 6, 'F': 3, 'H': 0, 'S': 9}
 
-    a = Vertex('A', heuristic_weight_dict['A'])
-    b = Vertex('B', heuristic_weight_dict['B'])
-    c = Vertex('C', heuristic_weight_dict['C'])
-    d = Vertex('D', heuristic_weight_dict['D'])
-    e = Vertex('E', heuristic_weight_dict['E'])
-    f = Vertex('F', heuristic_weight_dict['F'])
-    h = Vertex('H', heuristic_weight_dict['H'])
-    s = Vertex('S', heuristic_weight_dict['S'])
+    a = AStarGraphVertex('A', heuristic_weight_dict['A'])
+    b = AStarGraphVertex('B', heuristic_weight_dict['B'])
+    c = AStarGraphVertex('C', heuristic_weight_dict['C'])
+    d = AStarGraphVertex('D', heuristic_weight_dict['D'])
+    e = AStarGraphVertex('E', heuristic_weight_dict['E'])
+    f = AStarGraphVertex('F', heuristic_weight_dict['F'])
+    h = AStarGraphVertex('H', heuristic_weight_dict['H'])
+    s = AStarGraphVertex('S', heuristic_weight_dict['S'])
 
     input_graph.add_vertex(a)
     input_graph.add_vertex(b)
--- Tar4/dijkstra.py
+++ Tar4/dijkstra.py
@@ -1,27 +1,34 @@
+# Written by:
+# Sapir Holzman 318265097
+# Shahaf Shavit 314967324
+
+
 import sys
 import copy
 
-from Graph import Graph, Vertex 
+from Tar4.Graph import Graph, Vertex
+
 
 def dijkstra(graph, s):
     distance_for_adj, pi = initialize_single_source(graph, s)
-    S = Graph()
+    S = []
     q = copy.deepcopy(graph.vertices)
 
     while len(q) > 0:
         u = extract_min(q, distance_for_adj)
-        S.add_vertex(u)
+        S.append(u)
+
         for v in u.adjacent:
-            relax(u, graph[v["id"]], distance_for_adj, pi)
+            relax(u, graph.get_vertex(v['id']), distance_for_adj, pi)
 
     print_dijkstra_output(graph, s, pi, distance_for_adj)
 
 
 def initialize_single_source(graph, s):
-    distance_for_adj = { v: sys.maxsize for v in graph.keys()}
+    distance_for_adj = {v_id: sys.maxsize for v_id in graph.vertices_ids}
     distance_for_adj[s.id] = 0
 
-    pi = {v: None for v in graph.keys()}
+    pi = {v_id: None for v_id in graph.vertices_ids}
 
     return distance_for_adj, pi
 
@@ -29,12 +36,12 @@ def initialize_single_source(graph, s):
 def extract_min(q, distance_for_adj):
     min_vertex = None
     min_distance = sys.maxsize
-    
-    for v in q:
-        if distance_for_adj[v] < min_distance:
-            min_distance = distance_for_adj[v]
-            min_vertex = q[v]
-    if min_vertex.id in(q):    
+
+    for v_id in q.keys():
+        if distance_for_adj[v_id] < min_distance:
+            min_distance = distance_for_adj[v_id]
+            min_vertex = q[v_id]
+    if min_vertex:
         del q[min_vertex.id]
 
     return min_vertex
@@ -45,19 +52,20 @@ def relax(u, v, distance_for_adj, pi):
         distance_for_adj[v.id] = distance_for_adj[u.id] + u.get_adj_weight(v)
         pi[v.id] = u
 
+
 def print_dijkstra_output(graph, s, pi, distance_for_adj):
-    print('dijkstra results - find shortest paths from root {}'.format(s.id))
+    print('dijkstra results - find shortest paths from source vertex \'{}\': \n'.format(s.id))
 
-    for v in graph.keys():
-        if v != s.id:
-            print('vertex {} has distance of {} from the root {} and pi: {}.'
-                .format(v, distance_for_adj[v], s.id, pi[v].id))
+    for v_id in graph.vertices_ids:
+        if v_id != s.id:
+            print('Vertex \'{}\' has distance of {} from the source and pi: \'{}\'.'
+                  .format(v_id, distance_for_adj[v_id], pi[v_id].id))
         else:
-            print('vertex {} is root vertex and has distance of {} from himself and {} pi.'
-                .format(v, distance_for_adj[v], pi[v]))
-    print('The End :)')
+            print('vertex \'{}\' is the source vertex and has distance of {} from itself and \'{}\' pi.'
+                  .format(v_id, distance_for_adj[v_id], pi[v_id]))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     input_graph = Graph()
 
     a = Vertex('A')
